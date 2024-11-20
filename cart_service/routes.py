eebdbfaaf0f223a5fcb4f5178aa6489e18f89713
frank <gokofrank432@gmail.com>
2024-11-21 00:12:49 +0300

    ✨Restructured the models and schema🚀

--- cart_service/routes.py
+++ cart_service/routes.py
@@ -1,19 +1,23 @@
-from fastapi import status
+from fastapi import status, HTTPException
 from fastapi.routing import APIRouter
 from fastapi.responses import JSONResponse
 from pymongo import errors
 
-from .models import Cart_Items
-from .schema import Cart_Items_IN, CartItems_ResponseModel
-from .input_validators import validate_cart_items
+from .models import CartItemsModel
+from .schema import AddToCart, CartResponseModel, GetCart
+from .validators import validate_cart_items
+from .validators import validate_offset_limit
 
 cart_service = APIRouter(prefix="/cart", tags=["CART MICROSERVICE"])
 
 
+# USER
 @cart_service.post("")
-async def add_to_cart(item: Cart_Items_IN):
+async def add_to_cart(item: AddToCart):
     validate_cart_items(item)
-    cart_item = Cart_Items(**item.model_dump())
+    cart_item = CartItemsModel(
+        username=item.username, items=[item for item in item.items]
+    )
     try:
         await cart_item.insert()
     except errors.DuplicateKeyError:
@@ -31,12 +35,35 @@ async def add_to_cart(item: Cart_Items_IN):
     )
 
 
-@cart_service.get("", response_model=CartItems_ResponseModel)
-async def get_all_cart_items():
-    """gets all cart items for the user on the db
-
-    Returns:
-        JSON
-    """
-    cart_items = await Cart_Items.all().to_list()
+# ADMIN
+@cart_service.get("{limit}/{offset}", response_model=CartResponseModel)
+async def filter_cart_items(offset: int = 0, limit: int = 10):
+    validate_offset_limit(offset=offset, limit=limit)
+    cart_items = await CartItemsModel.find_many(skip=offset, limit=limit).to_list()
     return {"data": cart_items}
+
+
+# USER
+@cart_service.get("{username}", response_model=GetCart)
+async def filter_cart_item(username: str | None = None):
+    if username is not None:
+        cart = await CartItemsModel.find_one(username=username)
+        existing_username = cart.username
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"error": "Username is NULL!"},
+        )
+    if existing_username:
+        if cart.items:
+            return cart
+        else:
+            HTTPException(
+                status_code=status.HTTP_204_NO_CONTENT,
+                detail={"error": "The cart is EMPTY!!"},
+            )
+    else:
+        HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"error": "The user does not EXIST!!"},
+        )
